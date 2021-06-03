73144edaa09d71b9044a8428a391d00999e64dc3
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-06-03 16:42:41 +0900

    Master 02

--- src/CH01/C01Master.java
+++ src/CH01/C01Master.java
@@ -3,7 +3,8 @@ package CH01;
 public class C01Master {
 
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("Master Test 01");
+		System.out.println("Master Test 02");
 
 	}
 
